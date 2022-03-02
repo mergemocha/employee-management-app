fd1a3d29e75c3a5b2dd1cd398999120d149301bd
Linus Willner <hello@linuswillner.me>
2022-03-02 14:04:40 +0200

    Fix host updater

--- scripts/update-hosts.ps1
+++ scripts/update-hosts.ps1
@@ -1,2 +1,2 @@
 # Requires -RunAsAdministrator
-Add-Content $env:SYSTEMROOT\System32\drives\etc\hosts "# Added for employee-management-app-backend`n127.0.0.1 mongodb-primary`n127.0.0.1 mongodb-replica`n# End of section"
+Add-Content $env:SYSTEMROOT\System32\drivers\etc\hosts "# Added for employee-management-app-backend`n127.0.0.1 mongodb-primary`n127.0.0.1 mongodb-replica`n# End of section"
