b8c00f098aee90dd1292580ff6783456d3b3ef47
Anna Smati <anna.smati@gmail.com>
2022-03-07 12:05:55 +0200

    Add logout handling

--- src/api/v1/authorization/routes/logout.ts
+++ src/api/v1/authorization/routes/logout.ts
@@ -1,5 +1,20 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { getSession, terminateSession } from '../../../../security/session'
 
-export default async (req: FastifyRequest<{ Params: { username: string } }>, res: FastifyReply): Promise<void> => {
+export default async (req: FastifyRequest < { Body: {} }>, res: FastifyReply): Promise<void> => {
+  if (!req.headers.authorization) {
+    await res.status(500).send({ message: 'Error during logout' })
+    logger.error('Authorization header not present')
+  } else {
+    const session = await getSession(req.headers.authorization)
 
+    if (!session) {
+      await res.status(500).send({ message: 'Error during logout' })
+      logger.error('Session not found')
+    } else {
+      await terminateSession(session)
+      await res.status(200).send({ message: 'Logout successful' })
+      logger.info(`Session ${session.id} terminated`)
+    }
+  }
 }
