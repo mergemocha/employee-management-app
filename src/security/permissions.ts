b1ff89259353f7f20cde76ace9038410bba7e355
Linus Willner <hello@linuswillner.me>
2022-03-07 15:53:01 +0200

    Add some utility functions to permissions

--- src/security/permissions.ts
+++ src/security/permissions.ts
@@ -4,25 +4,33 @@ export type Permission = `read:employee.${keyof Employee}`
 
 let available: Permission[]
 
-try {
-  if (!process.env.RESTRICTED_FIELDS) {
-    throw new Error('Required environment variable RESTRICTED_FIELDS not present')
+export function load (): void {
+  try {
+    if (!process.env.RESTRICTED_FIELDS) {
+      throw new Error('Required environment variable RESTRICTED_FIELDS not present')
+    }
+
+    const arr = JSON.parse(process.env.RESTRICTED_FIELDS)
+
+    if (!Array.isArray(arr)) {
+      throw new Error('Environment variable RESTRICTED_FIELDS must be an array')
+    } else if (arr.some(elem => typeof elem !== 'string')) {
+      throw new Error('Environment variable RESTRICTED_FIELDS must be an array of strings')
+    }
+
+    available = fmtKeysToPermissions(arr)
+  } catch (err) {
+    logger.fatal(`Could not parse restricted fields: ${err}`)
+    process.exit(1)
   }
+}
 
-  const arr = JSON.parse(process.env.RESTRICTED_FIELDS)
+export function fmtKeysToPermissions (keys: Array<keyof Employee>): Permission[] {
+  return keys.map(key => convertKeyToPermission(key))
+}
 
-  if (!Array.isArray(arr)) {
-    throw new Error('Environment variable RESTRICTED_FIELDS must be an array')
-  } else if (arr.some(elem => typeof elem !== 'string')) {
-    throw new Error('Environment variable RESTRICTED_FIELDS must be an array of strings')
-  }
-
-  // Disabling this because it causes a false flag
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
-  available = arr.map(field => `read:employee.${field}` as Permission)
-} catch (err) {
-  logger.fatal(`Could not parse restricted fields: ${err}`)
-  process.exit(1)
+export function convertKeyToPermission (key: keyof Employee): Permission {
+  return `read:employee.${key}`
 }
 
 export const getAvailablePermissions = (): Permission[] => available
