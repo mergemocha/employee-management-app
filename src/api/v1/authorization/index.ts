d6e45014b3851e916eb86631bbc5f9a0920163a8
Anna Smati <anna.smati@gmail.com>
2022-03-07 12:05:38 +0200

    Update auth middlewares to include token validation

--- src/api/v1/authorization/index.ts
+++ src/api/v1/authorization/index.ts
@@ -1,9 +1,10 @@
 import { FastifyInstance, FastifyPluginOptions } from 'fastify'
+import { requiresAuth } from '../../middlewares'
 
 import login from './routes/login'
 import logout from './routes/logout'
 
 export default async (fastify: FastifyInstance, options: FastifyPluginOptions): Promise<void> => {
-  fastify.post('/login', login)
-  fastify.get('/logout', logout)
+  fastify.post('/login', { ...requiresAuth(fastify), handler: login })
+  fastify.post('/logout', { ...requiresAuth(fastify), handler: logout })
 }
