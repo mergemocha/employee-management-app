10da17fd2ef319fe3487b88f040879b854aaf3e4
Anna Smati <anna.smati@gmail.com>
2022-03-07 11:55:18 +0200

    Fix wrong route prefix

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -2,12 +2,12 @@
 import { FastifyInstance, FastifyPluginOptions } from 'fastify'
 
 import permissions from './permissions'
-import login from './authorization'
+import authorization from './authorization'
 import register from './register'
 
 export default async (fastify: FastifyInstance, options: FastifyPluginOptions): Promise<void> => {
   await fastify
     .register(permissions, { prefix: '/permissions' })
-    .register(login, { prefix: '/login' })
+    .register(authorization, { prefix: '/auth' })
     .register(register, { prefix: '/register' })
 }
