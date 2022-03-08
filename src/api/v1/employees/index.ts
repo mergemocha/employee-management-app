8b2311abaa813d1fba37d06b3180e50e93d0c16f
panueronen <panu.eronen@gmail.com>
2022-03-08 13:09:52 +0200

    renamed routes for clarity

--- src/api/v1/employees/index.ts
+++ src/api/v1/employees/index.ts
@@ -1,12 +1,13 @@
 import { FastifyInstance, FastifyPluginOptions } from 'fastify'
+import { requiresAuth, requiresSuperuser } from '../../middlewares'
 import employeesCreate from './routes/employees.create'
 import employeesDelete from './routes/employees.delete'
 import employeesGet from './routes/employees.get'
 import employeesUpdate from './routes/employees.update'
 
 export default async (fastify: FastifyInstance, options: FastifyPluginOptions): Promise<void> => {
-  fastify.get('/get', employeesGet)
-  fastify.post('/create', employeesCreate)
-  fastify.post('/delete', employeesDelete)
-  fastify.post('/update', employeesUpdate)
+  fastify.get('/get', { ...requiresAuth(fastify), handler: employeesGet })
+  fastify.post('/create', { ...requiresSuperuser(fastify), handler: employeesCreate })
+  fastify.post('/delete', { ...requiresSuperuser(fastify), handler: employeesDelete })
+  fastify.post('/update', { ...requiresSuperuser(fastify), handler: employeesUpdate })
 }
