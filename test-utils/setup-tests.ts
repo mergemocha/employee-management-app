1dcc7d692bb072a7ccb3641e7702e4ce48cd43e8
Linus Willner <hello@linuswillner.me>
2022-03-08 15:03:08 +0200

    Allow empty dotenv values

--- test-utils/setup-tests.ts
+++ test-utils/setup-tests.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv-safe'
 import pino from 'pino'
 
 export default (): void => {
-  dotenv.config()
+  dotenv.config({ allowEmptyValues: true })
 
   global.logger = pino({
     level: process.env.LOG_LEVEL,
