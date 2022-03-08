c30f6249c35e871bcceb85fe457fc4e51583d8cb
Linus Willner <hello@linuswillner.me>
2022-03-08 15:29:43 +0200

    Fix test env

--- test-utils/setup-tests.ts
+++ test-utils/setup-tests.ts
@@ -2,10 +2,10 @@ import dotenv from 'dotenv'
 import pino from 'pino'
 
 export default (): void => {
-  dotenv.config()
+  dotenv.config({ path: 'test.env' })
 
   global.logger = pino({
-    level: process.env.LOG_LEVEL ?? 'debug',
+    level: process.env.LOG_LEVEL,
     transport: {
       target: process.env.NODE_ENV !== 'production' ? 'pino-pretty' : ''
     }
