1e54089d37548292632d0569a562be5a03695277
Linus Willner <hello@linuswillner.me>
2022-03-09 13:56:02 +0200

    Delete old routes

--- src/api/v1/employees/routes/employees.get.ts
+++
@@ -1,6 +0,0 @@
-import { FastifyReply, FastifyRequest } from 'fastify'
-
-export default async (req: FastifyRequest, res: FastifyReply): Promise<void> => {
-  const employee = await prisma.employee.findMany()
-  await res.send(employee)
-}
